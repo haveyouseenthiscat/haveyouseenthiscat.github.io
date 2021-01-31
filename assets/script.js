bf2c45b98bcdd7770fcaf4fc8604739c0251b1a2
Virpi <1564285+viipu@users.noreply.github.com>
2021-01-31 14:58:42 +0200

    images displayed

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,5 @@
-function setCookie2(cname, cvalue) {
+function setCookie(cname, cvalue) {
   document.cookie = cname + "=" + cvalue;
-}
-function setCookie(cname, cvalue, exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  var expires = "expires="+ d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 function getCookie(cname) {
   var name = cname + "=";
@@ -22,17 +16,13 @@ function getCookie(cname) {
   }
   return "";
 }
-function checkPawca() {
-  var pawca = getCookie("pawcahontas");
-  if (pawca != "") {
-    alert("Veronica Maus");
+function checkVisited(cname, char, chartext) {
+  var cooky = getCookie(cname);
+  if (cooky != "") {
+    show(char, chartext);
   } else {
-    alert("The lamp is very dusty, but nothing interesting here just now.");
+    alert("Nothing to see here...yet?");
   }
-}
-function setPawca() {
-  document.cookie="pawcahontas=true";
-  show
 }
 function show(elem1, elem2) {
   var x = document.getElementById(elem1);
@@ -45,22 +35,32 @@ function hide(elem1, elem2) {
   x.style.display = "none";
   var y = document.getElementById(elem2);
   y.style.display = "none";
-
+}
+function switchImage(img1, img2) { 
+  var x = document.getElementById(img1);
+  x.style.display = "none";
+  var y = document.getElementById(img2);
+  y.style.display = "block";
 }
 function clickA1() {
   show('bandw','bandwtext');}
 function clickA2() {
-  show('vero','verotext');}
+//  show('vero','verotext');} //local testing
+  checkVisited('pawcahontas','vero','verotext');}
 function clickA3() {}
 function clickB1() {
   show('hiss','hisstext');}
 function clickB2() {
+  setCookie("pawcahontas", "true");
   show('pawca','pawcatext');}
 function clickB3() {
   show('frida','fridatext');}
 function clickC1() {
-  show('nyancy','nyancytext');}
+//  show('nyancy','nyancytext');} //local testing
+  checkVisited('purrot','nyancy','nyancytext');}
 function clickC2() {
+  setCookie("purrot", "true");
   show('purrot','purrottext');}
 function clickC3() {
+  setCookie("holmeows", "true");
   show('holm','holmtext');}
